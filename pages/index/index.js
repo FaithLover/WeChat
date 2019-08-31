59ebfaa474f5ae461832be1ff1f7fb1b062dfcbb
FaithSmile <wx_03f3824994424bfeb3ed0b931077bae9@git.code.tencent.com>
2019-08-31 10:48:29 +0800

    00-小程序初体验

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,5 +2,23 @@
 
 Page({
   data: {
-  }
+    name: 'xueyan',
+    age: 3,
+    students: [
+      { id: 1, name: 'ly', age: 18 },
+      { id: 2, name: 'lxy', age: 18 },
+      { id: 3, name: 'xzc', age: 8 }
+    ],
+    count: 0
+  },
+  up() {
+    this.setData({
+      count: this.data.count + 1
+    })
+  },
+  down() {
+    this.setData({
+      count: this.data.count - 1
+    })
+  },
 })
