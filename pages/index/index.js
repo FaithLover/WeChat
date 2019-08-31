2881f1e42f4cb1a55c4d653ac2e3d40a6bd7f7c2
FaithSmile <wx_03f3824994424bfeb3ed0b931077bae9@git.code.tencent.com>
2019-08-31 11:11:04 +0800

    01--swiper组件轮播图

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,5 +2,31 @@
 
 Page({
   data: {
+    imgUrls: [
+      'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+    ],
+    indicatorDots: true, //  是否显示面板指示点
+    autoplay: false, // 是否自动切换
+    circular: true, // 是否采用衔接滑动
+    current: 0, // 当前所在页面的 index
+    interval: 500, // 自动切换时间间隔
+    duration: 500 // 滑动动画时长
+  },
+  testDetails(e) {
+    // bindchange事件
+    console.log(e)
+  },
+  nextPage() {
+    // 跳转下一题
+    var current = this.data.current;
+    current++;
+    if (current > 2) {
+      current = 0
+    }
+    this.setData({
+      current: current
+    })
   }
 })
