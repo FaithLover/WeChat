b836fd69ae478ac35be895adf067586553b936cd
FaithSmile <wx_03f3824994424bfeb3ed0b931077bae9@git.code.tencent.com>
2019-08-31 11:42:02 +0800

    04--cover-view/image覆盖

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,13 @@
 //index.js
 
 Page({
-  data: {
+  onReady() {
+    this.videoCtx = wx.createVideoContext('myVideo')
+  },
+  play() {
+    this.videoCtx.play()
+  },
+  pause() {
+    this.videoCtx.pause()
   }
 })
