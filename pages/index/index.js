3da9dd26c85354bf63376cb6daeb414e0a5e038a
FaithSmile <wx_03f3824994424bfeb3ed0b931077bae9@git.code.tencent.com>
2019-09-02 15:59:42 +0800

    Mustache语法判断

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,5 +2,25 @@
 
 Page({
   data: {
+    message:"哦咯哦",
+    firstname:"第一个",
+    lastname:"最后一个",
+    age:18,
+    nowtime:new Date().toLocaleString(),
+    isAcitve:false
+  },
+  onLoad(){
+    setInterval(() => {
+      this.setData({
+        nowtime:new Date().toLocaleString()
+      })
+    },1000)
+  },
+  switchColor(){
+    console.log(1)
+    this.setData({
+        isAcitve:!this.data.isAcitve
+    })
+    console.log(this.data.isAcitve)
   }
 })
