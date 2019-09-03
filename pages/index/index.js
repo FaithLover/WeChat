b3ae15301cd97638c27653f5fa1fea97d2d8c035
FaithSmile <wx_03f3824994424bfeb3ed0b931077bae9@git.code.tencent.com>
2019-09-03 17:15:02 +0800

    小程序常用事件，数据传递，冒泡捕获

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,5 +2,36 @@
 
 Page({
   data: {
+    price:2.66888,
+    time:1560598160,
+    titles:["衣服","鞋子","裤子"]
+  },
+  handleTouchStart(even){
+    console.log(even.target)
+    console.log("handleTouchStart")
+  },
+  handleTouchMove() {
+    console.log("handleTouchMove")
+  },
+  handleTouchEnd() {
+    console.log("handleTouchEnd")
+  },
+  handleTap() {
+    console.log("handleTap")
+  },
+  handleLongPress() {
+    console.log("handleLongPress")
+  },
+  getVal(even){
+    console.log(even)
+  },
+  box1(){
+    console.log("box1")
+  },
+  box2() {
+    console.log("box2")
+  },
+  box3() {
+    console.log("box3")
   }
 })
