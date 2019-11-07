463d6a0f25717082e45098ef9f74ae7e53f3cf6c
努力做个没感情的戏子 <wx_03f3824994424bfeb3ed0b931077bae9@git.code.tencent.com>
2019-11-07 17:59:17 +0800

    页面跳转,数据传递

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,5 +2,23 @@
 
 Page({
   data: {
+    title:'哈哈哈'
+  },
+  handleAbout(){
+    wx.navigateTo({
+      url: '/pages/about/about',
+      success: function (res) {
+        console.log(res)
+      },
+      fail: function (res) {
+        console.log(res)
+      }
+    })
+    // navigator组件有多少种方法跳转，函数就有多少种跳转方法
+    // wx.redirectTo({
+    //   url: ''
+    // })
   }
+  
+
 })
