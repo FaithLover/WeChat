f666a359b43a640e2b95a32feebee0e4696521fc
努力做个没感情的戏子 <wx_03f3824994424bfeb3ed0b931077bae9@git.code.tencent.com>
2019-11-07 10:16:31 +0800

    用户登录流程

--- app.js
+++ app.js
@@ -1,39 +1,68 @@
 //app.js
+// 登录操作
 App({
+  globalData:{
+    token:''
+  },
+  //完成加载一次
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
     // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
+    const token = wx.getStorageSync("token")
+    if(token && token.length !==0){
+      this.check_token(token)
+    }else{
+      this.login()
+    }
+    
+  },
+  check_token(token){
+    console.log("验证token操作")
+    wx.request({
+      url: 'http://123.207.32.32:3000/auth',
+      method:'post',
+      header:{
+        token
+      },
+      success:(res)=>{
+        console.log(res)
+        if(!res.data.errCode){
+          console.log("token有效")
+          this.globalData.token = token;
+        }else{
+          this.login()
         }
+      },
+      fail:(res)=>{
+        console.log(res)
       }
     })
   },
-  globalData: {
-    userInfo: null
+  login() {
+    console.log("执行登录操作")
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        console.log(res)
+        const code = res.code
+        wx.request({
+          url: 'http://123.207.32.32:3000/login',
+          data: { code },
+          method: 'POST',
+          success: (res) => {
+            console.log(res)
+            const token = res.data.token;
+            this.globalData.token = token
+            // 异步执行
+            // wx.setStorage({
+            //   key: '',
+            //   data: '',
+            // })
+            //同步执行，执行完再执行下一步
+            wx.setStorageSync("token", token)
+          },
+          fail: function (res) { }
+        })
+      }
+    })
   }
 })
